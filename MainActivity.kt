ea20e779922d9bc8fd92a8610cfa4dadf43be0d3
ShayaanT <95000377+ShayaanT@users.noreply.github.com>
2022-07-17 06:12:40 -0400

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,11 +1,81 @@
 package com.example.messagingapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var userrecyclerview: RecyclerView
+    private lateinit var userlist: ArrayList<user>
+    private lateinit var adapter: userAdapter
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mDbRef: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mAuth = FirebaseAuth.getInstance()
+        mDbRef = FirebaseDatabase.getInstance().getReference()
+
+        userlist = ArrayList()
+        adapter = userAdapter(this, userlist)
+
+        userrecyclerview = findViewById(R.id.userrecyclerview)
+
+        userrecyclerview.layoutManager = LinearLayoutManager(this)
+        userrecyclerview.adapter = adapter
+
+        mDbRef.child("user").addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+
+                userlist.clear()
+                for (postsnapshot in snapshot.children) {
+
+
+                    val currentuser = postsnapshot.getValue(user::class.java)
+
+                    if(mAuth.currentUser?.uid != currentuser?.uid){
+                        userlist.add(currentuser!!)
+                    }
+                }
+
+
+
+                adapter.notifyDataSetChanged()
+
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
+
+        fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+        return super.onCreateOptionsMenu(menu)
     }
-}
+        fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(item.itemId == R.id.logout){
+            //write logic for logout
+            mAuth.signOut()
+            val intent = Intent(this@MainActivity, Login::class.java)
+            finish()
+            startActivity(intent)
+            return true
+        }
+        return true
+
+    }
+
+}}
