bd76adadfb9878453c962ddf6951ebe1c71d4101
ShayaanT <95000377+ShayaanT@users.noreply.github.com>
2022-07-16 22:11:40 -0400

    Update Login.kt

--- Login.kt
+++ Login.kt
@@ -1,1 +1,41 @@
+ckage com.example.messagingapp
 
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.ContactsContract
+import android.widget.Button
+import android.widget.EditText
+
+class Login : AppCompatActivity() {
+
+    private lateinit var etemail: EditText
+    private lateinit var etpassword: EditText
+    private lateinit var btnlogin: Button
+    private lateinit var btnsignup: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        etemail = findViewById(R.id.etemail)
+        etpassword = findViewById(R.id.etpassword)
+        btnlogin = findViewById(R.id.btnlogin)
+        btnsignup = findViewById(R.id.btnsignup)
+
+        btnsignup.setOnClickListener {
+            val intent = Intent(this, signup::class.java)
+            startActivity(intent)
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
